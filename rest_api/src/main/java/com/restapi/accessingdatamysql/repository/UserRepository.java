1c26518259ddba536a3ff2c5fd62888799e5b5f4
Envyw6567 <nbw970508@gmail.com>
2023-01-24 23:01:31 +0900

    Project Init [--Security using JWT--]

--- rest_api/src/main/java/com/restapi/accessingdatamysql/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.restapi.accessingdatamysql.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.restapi.accessingdatamysql.domain.User;
-
-public interface UserRepository  extends CrudRepository <User, Integer>{
-
-}
