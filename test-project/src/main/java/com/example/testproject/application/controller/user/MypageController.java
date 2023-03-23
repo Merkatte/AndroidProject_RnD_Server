6ac455ac7fc1d695eed58c6d1d67c8030360eb98
Envyw6567 <nbw970508@gmail.com>
2023-03-23 20:28:08 +0900

    [마이페이지 나의 판매 목록 filter 적용 (모든, 판매 완료, 판매 중) ]

--- test-project/src/main/java/com/example/testproject/application/controller/user/MypageController.java
+++ test-project/src/main/java/com/example/testproject/application/controller/user/MypageController.java
@@ -1,2 +1,27 @@
-package com.example.testproject.application.controller.user;public class MypageController {
+package com.example.testproject.application.controller.user;
+
+import com.example.testproject.domain.auction.dto.response.MypageBidsListResponseDTO;
+import com.example.testproject.domain.auction.entity.AuctionItems;
+import com.example.testproject.domain.user.service.MypageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/mypage")
+@RequiredArgsConstructor
+public class MypageController {
+
+    private final MypageService mypageService;
+    @GetMapping("/bid-list/{userId}")
+    public List<MypageBidsListResponseDTO> getMypageBids(@PathVariable("userId") Long bidderId){
+        return mypageService.getMypageBids(bidderId);
+    }
+
+    @GetMapping("/auction-list/{userId}")
+    public List<AuctionItems> getMypageAuctionItems(@PathVariable("userId") Long userId, @RequestParam("filter") String filter){
+        return mypageService.getMypageAuctionItems(userId, filter);
+    }
+
 }
